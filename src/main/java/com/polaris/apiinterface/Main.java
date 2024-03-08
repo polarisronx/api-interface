0a2b731f907d778538dabd4d5887147614b4009c
Polarisronx <polarisronx@163.com>
2024-03-08 16:08:58 +0800

    7th-commit:api-client

---
+++ src/main/java/com/polaris/apiinterface/Main.java
@@ -0,0 +1,25 @@
+package com.polaris.apiinterface;
+
+import com.polaris.apiinterface.client.ApiClient;
+import com.polaris.apiinterface.model.User;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author Administrator
+ * @Create 2024-03-08 15:47
+ * @Version 1.0
+ * ClassName Main
+ * Package com.polaris.apiinterface
+ * Description
+ */
+@Slf4j
+public class Main {
+    public static void main (String[] args){
+        ApiClient apiClient = new ApiClient();
+        User user = new User();
+        user.setUsername("开心果3");
+        log.info("-----{}",apiClient.getNameByGet("开心果1"));
+        log.info("-----{}",apiClient.getNameByPost1("开心果2"));
+        log.info("-----{}",apiClient.getNameByPost2(user));
+    }
+}
---
+++ src/main/java/com/polaris/apiinterface/client/ApiClient.java
@@ -0,0 +1,56 @@
+package com.polaris.apiinterface.client;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.polaris.apiinterface.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+
+/**
+ * @Author polaris
+ * @Create 2024-03-08 15:24
+ * @Version 1.0
+ * ClassName ApiClient
+ * Package com.polaris.apiinterface.client
+ * Description
+ */
+@Slf4j
+public class ApiClient {
+    public String getNameByGet(String name){
+        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", name);
+        String result = HttpUtil.get("http://localhost:8123/api/name/a", paramMap);
+        log.info("get请求结果：{}", result);
+        return result;
+    }
+
+    public String getNameByPost1(String name){
+        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", name);
+        String result = HttpUtil.post("http://localhost:8123/api/name/b", paramMap);
+        log.info("post1请求结果：{}", result);
+        return result;
+    }
+
+    public String getNameByPost2(User user){
+        // 将User对象转换为JSON格式的字符串
+        String jsonStr = JSONUtil.toJsonStr(user);
+        // 使用HttpRequest库发送POST请求，并获取服务器的响应
+        HttpResponse httpResponse = HttpRequest.post("http://localhost:8123/api/name/c")
+                .body(jsonStr)// 将json字符串设置为请求体
+                .execute();// 执行请求
+        int status = httpResponse.getStatus();
+        String result = httpResponse.body();
+        log.info("status:{}",status);
+        log.info("post2请求结果：{}",result);
+        return result;
+    }
+}
